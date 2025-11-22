a8e90c4e45fd9ffe8779d21c0ed65ef0a8626e2f
eomayski <emil@omayski.com>
2025-11-22 16:11:43 +0200

    resources fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 
 function App() {
 
-  return (
-    <>
-      <p>
-        Hello
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>
+                Hello
+            </h1>
+        </>
+    )
 }
 
 export default App
